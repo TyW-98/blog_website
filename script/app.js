92631bdc64f5360eb2d5d2ddc41e941cf1cc1aa4
wey1998 <yitwey1998@hotmail.com>
2023-04-17 17:20:52 +0100

    Add all starting files

---
+++ script/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const https = require('https');
+const ejs = require('ejs');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('Test js file ')
+})
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
