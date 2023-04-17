798d6538ff025b2a04e325b06bdb76a4902fc943
wey1998 <yitwey1998@hotmail.com>
2023-04-17 18:00:36 +0100

    Add navbar and styling for navbar

--- script/app.js
+++ script/app.js
@@ -11,9 +11,10 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Test js file ')
+  res.render('header',{
+    
+  })
 })
-
 
 
 app.listen(3000, () => {
