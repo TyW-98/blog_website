65f0c5797f339ffd044f2acc15a546d7c36707b2
wey1998 <yitwey1998@hotmail.com>
2023-04-23 23:53:56 +0100

    Add defaultContentSchema for default content in home,about and contact page

--- script/app.js
+++ script/app.js
@@ -4,26 +4,61 @@ const path = require("path");
 const https = require("https");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const date = required("date-and-time");
+
+// mongoose.connect("mongodb://127.0.0.1:27017/blog-journals")
 
 const app = express();
-let postList = [];
+
+const defaultContentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+});
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  blogContent: {
+    type: String,
+    required: true,
+  },
+  dateCreated: {
+    type: String,
+    required: true,
+  },
+});
+
+const DefaultContent = new moongoose.Model("defaultContent", defaultContentSchema)
+const Post = new mongoose.Model("Post", postSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+const homePageDefaultContent = new DefaultContent({
+  content: "Amidst the hustle and bustle of the city, I found solace in the small things. The sound of rain tapping against the window, the smell of fresh coffee in the morning, and the sight of a rainbow after a storm. It's easy to get caught up in the chaos, but sometimes all it takes is a moment of mindfulness to appreciate the beauty around us.";
+})
 
-const homeStartingContent =
-  "Amidst the hustle and bustle of the city, I found solace in the small things. The sound of rain tapping against the window, the smell of fresh coffee in the morning, and the sight of a rainbow after a storm. It's easy to get caught up in the chaos, but sometimes all it takes is a moment of mindfulness to appreciate the beauty around us.";
-const aboutContent =
-  "At our company, we're passionate about providing our customers with high-quality products and services. We believe that our success is directly tied to the satisfaction of our customers, so we work hard every day to exceed their expectations. Our team is made up of experienced professionals who are committed to delivering exceptional results, and we're always striving to improve our processes and refine our approach. Whether you're a new customer or a longtime partner, you can count on us to provide the support and solutions you need to achieve your goals. Thank you for choosing us as your partner in success!";
-const contactContent =
-  "Thank you for visiting my website! If you have any questions, comments, or would like to collaborate on a project, please don't hesitate to reach out. I am always interested in connecting with like-minded individuals and exploring new opportunities. You can contact me directly through the form below, and I will get back to you as soon as possible. I look forward to hearing from you!";
+const aboutPageDefaultContent = new DefaultContent({
+  content:  "At our company, we're passionate about providing our customers with high-quality products and services. We believe that our success is directly tied to the satisfaction of our customers, so we work hard every day to exceed their expectations. Our team is made up of experienced professionals who are committed to delivering exceptional results, and we're always striving to improve our processes and refine our approach. Whether you're a new customer or a longtime partner, you can count on us to provide the support and solutions you need to achieve your goals. Thank you for choosing us as your partner in success!";
+})
+
+const contactPageDefaultContent = new DefaultContent({
+  content:  "Thank you for visiting my website! If you have any questions, comments, or would like to collaborate on a project, please don't hesitate to reach out. I am always interested in connecting with like-minded individuals and exploring new opportunities. You can contact me directly through the form below, and I will get back to you as soon as possible. I look forward to hearing from you!";
+})
+
+
 
 app.get("/", (req, res) => {
   res.render("home", {
     homeStartingContent: homeStartingContent,
-    postList: postList
+    postList: postList,
   });
 });
 
