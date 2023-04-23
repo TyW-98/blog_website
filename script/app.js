d2955fa337f89f33781e30787260bda54cb7a49d
wey1998 <yitwey1998@hotmail.com>
2023-04-24 00:02:26 +0100

    Remove defaultContentSchema, as it is not necessary

--- script/app.js
+++ script/app.js
@@ -12,13 +12,6 @@ const date = required("date-and-time");
 
 const app = express();
 
-const defaultContentSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    required: true,
-  },
-});
-
 const postSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -41,61 +34,62 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-const homePageDefaultContent = new DefaultContent({
-  content: "Amidst the hustle and bustle of the city, I found solace in the small things. The sound of rain tapping against the window, the smell of fresh coffee in the morning, and the sight of a rainbow after a storm. It's easy to get caught up in the chaos, but sometimes all it takes is a moment of mindfulness to appreciate the beauty around us.";
-})
+const homePageDefaultContent = "Amidst the hustle and bustle of the city, I found solace in the small things. The sound of rain tapping against the window, the smell of fresh coffee in the morning, and the sight of a rainbow after a storm. It's easy to get caught up in the chaos, but sometimes all it takes is a moment of mindfulness to appreciate the beauty around us.";
+const aboutPageDefaultContent = "At our company, we're passionate about providing our customers with high-quality products and services. We believe that our success is directly tied to the satisfaction of our customers, so we work hard every day to exceed their expectations. Our team is made up of experienced professionals who are committed to delivering exceptional results, and we're always striving to improve our processes and refine our approach. Whether you're a new customer or a longtime partner, you can count on us to provide the support and solutions you need to achieve your goals. Thank you for choosing us as your partner in success!";
+const contactPageDefaultContent = "Thank you for visiting my website! If you have any questions, comments, or would like to collaborate on a project, please don't hesitate to reach out. I am always interested in connecting with like-minded individuals and exploring new opportunities. You can contact me directly through the form below, and I will get back to you as soon as possible. I look forward to hearing from you!";
 
-const aboutPageDefaultContent = new DefaultContent({
-  content:  "At our company, we're passionate about providing our customers with high-quality products and services. We believe that our success is directly tied to the satisfaction of our customers, so we work hard every day to exceed their expectations. Our team is made up of experienced professionals who are committed to delivering exceptional results, and we're always striving to improve our processes and refine our approach. Whether you're a new customer or a longtime partner, you can count on us to provide the support and solutions you need to achieve your goals. Thank you for choosing us as your partner in success!";
-})
-
-const contactPageDefaultContent = new DefaultContent({
-  content:  "Thank you for visiting my website! If you have any questions, comments, or would like to collaborate on a project, please don't hesitate to reach out. I am always interested in connecting with like-minded individuals and exploring new opportunities. You can contact me directly through the form below, and I will get back to you as soon as possible. I look forward to hearing from you!";
-})
-
-
+const todaysDate = date.format(new Date(), 'DD MMM YYYY (dddd)')
 
 app.get("/", (req, res) => {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    postList: postList,
-  });
-});
+  Post.find().then((task) => {
+    if(task.length === 0) {
 
-app.get("/about", (req, res) => {
-  res.render("about", { aboutContent: aboutContent });
-});
-
-app.get("/contact", (req, res) => {
-  res.render("contact", { contactContent: contactContent });
-});
-
-app.get("/compose", (req, res) => {
-  res.render("compose", {});
-});
-
-app.get("/post/:blogId", (req, res) => {
-  let selectedPost = "";
-
-  for (let i = 0; i < postList.length; i++) {
-    if (_.lowerCase(postList[i].postTitle) === _.lowerCase(req.params.blogId)) {
-      selectedPost = postList[i];
-      break;
-    } else {
     }
-  }
-  res.render("post", { selectedPost: selectedPost });
-});
+  })
+})
 
-app.post("/compose", (req, res) => {
-  const newPost = {
-    postTitle: req.body.blogTitle,
-    postContent: req.body.blogContent,
-  };
-  res.redirect("/");
-  postList.push(newPost);
-});
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+// app.get("/", (req, res) => {
+//   res.render("home", {
+//     homeStartingContent: homeStartingContent,
+//     postList: postList,
+//   });
+// });
+
+// app.get("/about", (req, res) => {
+//   res.render("about", { aboutContent: aboutContent });
+// });
+
+// app.get("/contact", (req, res) => {
+//   res.render("contact", { contactContent: contactContent });
+// });
+
+// app.get("/compose", (req, res) => {
+//   res.render("compose", {});
+// });
+
+// app.get("/post/:blogId", (req, res) => {
+//   let selectedPost = "";
+
+//   for (let i = 0; i < postList.length; i++) {
+//     if (_.lowerCase(postList[i].postTitle) === _.lowerCase(req.params.blogId)) {
+//       selectedPost = postList[i];
+//       break;
+//     } else {
+//     }
+//   }
+//   res.render("post", { selectedPost: selectedPost });
+// });
+
+// app.post("/compose", (req, res) => {
+//   const newPost = {
+//     postTitle: req.body.blogTitle,
+//     postContent: req.body.blogContent,
+//   };
+//   res.redirect("/");
+//   postList.push(newPost);
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
